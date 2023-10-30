2729984de4343b82a411b776c5a6df7bf58049cc
Gaudenz@Krakatoom <42372680+Gaudenz77@users.noreply.github.com>
2023-10-30 14:08:08 +0100

    formatting +qr-code

--- process.php
+++ process.php
@@ -1,36 +1,53 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Include Bootstrap 5.3 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
-<body>
-<div class="container">
-    <div class="row justify-content-center align-items-center" style="height: 100vh;">
-        <div class="col-md-6">
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-                if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $to = 'gaudenz.raiber@gmail.com'; // Your email address
-                    $subject = 'New Email Subscription';
-                    $message = 'New email subscription: ' . $email;
-                    $headers = 'From: ' . $email;
 
-                    if (mail($to, $subject, $message, $headers)) {
-                        echo '<div class="alert alert-success" role="alert">Thank you for subscribing!</div>';
+<body>
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height: 100vh;">
+            <div class="col-md-6">
+
+                <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+                    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        $to = 'cvsenger@gmail.com'; // Your email address
+                        $subject = 'New Email Subscription';
+                        $message = 'New email subscription: ' . $email;
+                        $headers = 'From: ' . $email;
+
+                        if (mail($to, $subject, $message, $headers)) {
+                            echo '<div class="alert alert-success" role="alert">Thank you for subscribing!</div>';
+                        } else {
+                            echo '<div class="alert alert-danger" role="alert">Subscription failed. Please try again later.</div>';
+                        }
                     } else {
-                        echo '<div class="alert alert-danger" role="alert">Subscription failed. Please try again later.</div>';
+                        echo '<div class="alert alert-danger" role="alert">Invalid email address. Please enter a valid email.</div>';
                     }
-                } else {
-                    echo '<div class="alert alert-danger" role="alert">Invalid email address. Please enter a valid email.</div>';
                 }
-            }
-            ?>
-            <p><a href="index.html" class="btn btn-primary">Back to Homepage</a></p>
+                ?>
+
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <a href="index.html" class="btn btn-primary">Back to Homepage</a>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <img src="images/zap-bestehen-qr.png" class="img-fluid mt-3" alt="qr-code">
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
-</body>
-</html>
 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
--- processform.php
+++ processform.php
@@ -1,40 +1,58 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- Include Bootstrap 5.3 CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
+
 <body>
-<div class="container">
-    <div class="row justify-content-center align-items-center" style="height: 100vh;">
-        <div class="col-md-6">
-            <?php
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                $name = $_POST['name'];
-                $email = filter_var($_POST['_replyto'], FILTER_SANITIZE_EMAIL);
-                $message = $_POST['message'];
+    <div class="container">
+        <div class="row justify-content-center align-items-center" style="height: 100vh;">
+            <div class="col-md-6">
+                <?php
+                if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+                    $name = $_POST['name'];
+                    $email = filter_var($_POST['_replyto'], FILTER_SANITIZE_EMAIL);
+                    $message = $_POST['message'];
 
-                if (empty($name) || empty($email) || empty($message)) {
-                    echo '<div class="alert alert-danger" role="alert">All fields are required. Please fill out the form completely.</div>';
-                } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    echo '<div class="alert alert-danger" role="alert">Invalid email address. Please enter a valid email.</div>';
-                } else {
-                    $to = 'gaudenz.raiber@gmail.com'; // Your email address
-                    $subject = 'New Contact Form Submission';
-                    $message = "Name: $name\nEmail: $email\nMessage:\n$message";
-                    $headers = 'From: ' . $email;
-
-                    if (mail($to, $subject, $message, $headers)) {
-                        echo '<div class="alert alert-success" role="alert">Your message has been sent successfully!</div>';
+                    if (empty($name) || empty($email) || empty($message)) {
+                        echo '<div class="alert alert-danger" role="alert">All fields are required. Please fill out the form completely.</div>';
+                    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        echo '<div class="alert alert-danger" role="alert">Invalid email address. Please enter a valid email.</div>';
                     } else {
-                        echo '<div class="alert alert-danger" role="alert">Message delivery failed. Please try again later.</div>';
+                        $to = 'cvsenger@gmail.com'; // Your email address
+                        $subject = 'New Contact Form Submission';
+                        $message = "Name: $name\nEmail: $email\nMessage:\n$message";
+                        $headers = 'From: ' . $email;
+
+                        if (mail($to, $subject, $message, $headers)) {
+                            echo '<div class="alert alert-success" role="alert">Your message has been sent successfully!</div>';
+                        } else {
+                            echo '<div class="alert alert-danger" role="alert">Message delivery failed. Please try again later.</div>';
+                        }
                     }
                 }
-            }
-            ?>
-            <p><a href="index.html" class="btn btn-primary">Back to Homepage</a></p>
+                ?>
+
+                <div class="row justify-content-center">
+                    <div class="col-4">
+                        <a href="index.html" class="btn btn-primary">Back to Homepage</a>
+                    </div>
+                </div>
+                <div class="row justify-content-center">
+                    <div class="col-6">
+                        <img src="images/zap-bestehen-qr.png" class="img-fluid mt-3" alt="qr-code">
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
-</div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
+
 </html>
